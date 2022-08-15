602230c97343826af7d33fb8a997ca72236ebec6
Lautaro <lautiquiroga10@gmail.com>
2022-08-15 17:54:38 -0300

    Finished homework

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -60,32 +60,36 @@ function sonIguales(x, y) {
   // Devuelve "true" si "x" e "y" son iguales
   // De lo contrario, devuelve "false"
   // Tu código:
-  if (x === y) {return true};
-  return false;
+  if (x === y) {
+    return true;
+  } return false;
 }
 
 function tienenMismaLongitud(str1, str2) {
   // Devuelve "true" si las dos strings tienen la misma longitud
   // De lo contrario, devuelve "false"
   // Tu código:
-  if (str1 .length === str2 .length) {return true};
-  return false;
+  if (str1 .length === str2 .length) {
+    return true;
+  } return false;
 }
 
 function menosQueNoventa(num) {
   // Devuelve "true" si el argumento de la función "num" es menor que noventa
   // De lo contrario, devuelve "false"
   // Tu código:
-  if (num < 90) {return true};
-  return false;
+  if (num < 90) {
+    return true;
+  } return false;
 }
 
 function mayorQueCincuenta(num) {
   // Devuelve "true" si el argumento de la función "num" es mayor que cincuenta
   // De lo contrario, devuelve "false"
   // Tu código:
-  if (num > 50) {return true};
-  return false;
+  if (num > 50) {
+    return true;
+  } return false;
 }
 
 function obtenerResto(x, y) {
@@ -98,16 +102,18 @@ function esPar(num) {
   // Devuelve "true" si "num" es par
   // De lo contrario, devuelve "false"
   // Tu código:
-  if ((num%2)===0) {return true};
-  return false;
+  if ((num%2)===0) {
+    return true;
+  } return false;
 }
 
 function esImpar(num) {
   // Devuelve "true" si "num" es impar
   // De lo contrario, devuelve "false"
   // Tu código:
-  if ((num%2)>0) {return true};
-  return false;
+  if ((num%2)>0) {
+    return true;
+  } return false;
 }
 
 function elevarAlCuadrado(num) {
@@ -152,9 +158,13 @@ function esPositivo(numero) {
   //Si el número es positivo, devolver ---> "Es positivo"
   //Si el número es negativo, devolver ---> "Es negativo"
   //Si el número es 0, devuelve false
-  if (numero > 0) {return 'Es positivo'};
-  if (numero < 0) {return 'Es negativo'};
-  if (numero === 0) {return false};
+  if (numero > 0) {
+    return 'Es positivo';
+  } else if (numero < 0) {
+    return 'Es negativo';
+  } else {
+    return false;
+  }
 }
 
 function agregarSimboloExclamacion(str) {
@@ -213,12 +223,25 @@ function esVocal(letra){
   //que no se puede procesar el dato mediante el mensaje "Dato incorrecto".
   // Si no es vocal, tambien debe devolver "Dato incorrecto".
   //Escribe tu código aquí
-  if (letra === 'a') {return 'Es vocal'};
-  if (letra === 'e') {return 'Es vocal'};
-  if (letra === 'i') {return 'Es vocal'};
-  if (letra === 'o') {return 'Es vocal'};
-  if (letra === 'u') {return 'Es vocal'};
-  return 'Dato incorrecto';
+  switch(letra){
+    case 'a':
+      return 'Es vocal';
+      break;
+    case 'e':
+      return 'Es vocal';
+      break;
+    case 'i':
+      return 'Es vocal';
+      break;
+    case 'o':
+      return 'Es vocal';
+      break;
+    case 'u':
+      return 'Es vocal';
+      break;
+      default: 
+      return 'Dato incorrecto';
+  }
 }
 
 
